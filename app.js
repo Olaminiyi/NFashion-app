a38fc9988a29e0187d5ecbce7618a75d485289b0
Olaminiyi <olaiyaolaminiyi@gmail.com>
2020-06-01 17:37:29 +0100

    destructuring the product object using ES6 map

--- app.js
+++ app.js
@@ -13,7 +13,27 @@ const productsDOm = document.querySelector(".products-center");
 let cart = [];
 
 // getting the products
-class products {}
+class Products {
+  async getProducts() {
+    try {
+      // get the products using fetch method
+      let result = await fetch("products.json");
+      // get data in json format
+      let data = await result.json();
+      let products = data.items;
+      // using ES6 map function for destructuring the products objects
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 // display products
 class UI {}
 //local storage
@@ -21,5 +41,7 @@ class Storage {}
 
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
-  const products = new products();
+  const products = new Products();
+
+  products.getProducts().then((data) => console.log(data));
 });
